9454116695436b501606bf1a4fd2d2a07e91f375
Adam Reuben <adamreuben66@gmail.com>
2022-02-14 11:33:08 +0300

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBjLnOZ38TlWSU0QhE72k8_GlxbFJiGQTc",
-    authDomain: "atiproject-2cf9c.firebaseapp.com",
-    databaseURL: "https://atiproject-2cf9c.firebaseio.com",
-    projectId: "atiproject-2cf9c",
-    storageBucket: "atiproject-2cf9c.appspot.com",
-    messagingSenderId: "81781405614",
-    appId: "1:81781405614:web:f0095c42384e246ae2ba3f"
+    //PASTE UR FIREBASE CONFIGS HERE
   }
 };
