9d3a67f6f7484c75cbbe09e1f748866574c3b0ba
adamwreuben <adamreuben66@gmail.com>
2022-02-18 01:40:25 +0300

    add back firebase config files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    //PASTE UR CONFIGS
+    apiKey: "AIzaSyBjLnOZ38TlWSU0QhE72k8_GlxbFJiGQTc",
+    authDomain: "atiproject-2cf9c.firebaseapp.com",
+    databaseURL: "https://atiproject-2cf9c.firebaseio.com",
+    projectId: "atiproject-2cf9c",
+    storageBucket: "atiproject-2cf9c.appspot.com",
+    messagingSenderId: "81781405614",
+    appId: "1:81781405614:web:f0095c42384e246ae2ba3f"
   }
 };
 
