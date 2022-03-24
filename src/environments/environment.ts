93b0f976a3ed3b02432b892325ba6044db613249
Adam Reuben <adamreuben66@gmail.com>
2022-03-24 21:31:40 +0300

    af

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBjLnOZ38TlWSU0QhE72k8_GlxbFJiGQTc",
-    authDomain: "atiproject-2cf9c.firebaseapp.com",
-    databaseURL: "https://atiproject-2cf9c.firebaseio.com",
-    projectId: "atiproject-2cf9c",
-    storageBucket: "atiproject-2cf9c.appspot.com",
-    messagingSenderId: "81781405614",
-    appId: "1:81781405614:web:f0095c42384e246ae2ba3f"
-  }
-};
+//ADD UR OWN KEYS
 
 /*
  * For easier debugging in development mode, you can import the following file
